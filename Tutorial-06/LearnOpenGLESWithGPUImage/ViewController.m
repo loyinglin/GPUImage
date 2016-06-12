5b75ae20fbe2259fa9e18aa9b12f6c57c347b70c
林伟池 <loying@foxmail.com>
2016-06-12 11:26:49 +0800

    视频录制

---
+++ Tutorial-06/LearnOpenGLESWithGPUImage/ViewController.m
@@ -0,0 +1,161 @@
+//
+//  ViewController.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by 林伟池 on 16/5/10.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GPUImage.h"
+#import <AssetsLibrary/ALAssetsLibrary.h>
+
+@interface ViewController ()
+@property (nonatomic , strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic , strong) GPUImageOutput<GPUImageInput> *filter;
+@property (nonatomic , strong) GPUImageMovieWriter *movieWriter;
+@property (nonatomic , strong) GPUImageView *filterView;
+
+@property (nonatomic , strong) UIButton *mButton;
+@property (nonatomic , strong) UILabel  *mLabel;
+@property (nonatomic , assign) long     mLabelTime;
+@property (nonatomic , strong) NSTimer  *mTimer;
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    _videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    _videoCamera.horizontallyMirrorFrontFacingCamera = NO;
+    _videoCamera.horizontallyMirrorRearFacingCamera = NO;
+    
+    _filter = [[GPUImageSepiaFilter alloc] init];
+    _filterView = [[GPUImageView alloc] initWithFrame:self.view.frame];
+    self.view = _filterView;
+    
+    _mButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 10, 50, 50)];
+    [_mButton setTitle:@"录制" forState:UIControlStateNormal];
+    [_mButton sizeToFit];
+    [self.view addSubview:_mButton];
+    [_mButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _mLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 20, 50, 100)];
+    _mLabel.hidden = YES;
+    _mLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:_mLabel];
+    
+    
+    [_videoCamera addTarget:_filter];
+    [_filter addTarget:_filterView];
+    [_videoCamera startCameraCapture];
+}
+
+
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    // Map UIDeviceOrientation to UIInterfaceOrientation.
+    UIInterfaceOrientation orient = UIInterfaceOrientationPortrait;
+    switch ([[UIDevice currentDevice] orientation])
+    {
+        case UIDeviceOrientationLandscapeLeft:
+            orient = UIInterfaceOrientationLandscapeLeft;
+            break;
+            
+        case UIDeviceOrientationLandscapeRight:
+            orient = UIInterfaceOrientationLandscapeRight;
+            break;
+            
+        case UIDeviceOrientationPortrait:
+            orient = UIInterfaceOrientationPortrait;
+            break;
+            
+        case UIDeviceOrientationPortraitUpsideDown:
+            orient = UIInterfaceOrientationPortraitUpsideDown;
+            break;
+            
+        case UIDeviceOrientationFaceUp:
+        case UIDeviceOrientationFaceDown:
+        case UIDeviceOrientationUnknown:
+            // When in doubt, stay the same.
+            orient = fromInterfaceOrientation;
+            break;
+    }
+    _videoCamera.outputImageOrientation = orient;
+    
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES; // Support all orientations.
+}
+
+- (void)onTimer:(id)sender {
+    _mLabel.text = [NSString stringWithFormat:@"录制时间:%lds", _mLabelTime++];
+    [_mLabel sizeToFit];
+}
+
+- (void)onClick:(UIButton *)sender {
+    NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie.m4v"];
+    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+    if([sender.currentTitle isEqualToString:@"录制"]) {
+        [sender setTitle:@"结束" forState:UIControlStateNormal];
+        NSLog(@"Start recording");
+        unlink([pathToMovie UTF8String]); // 如果已经存在文件，AVAssetWriter会有异常，删除旧文件
+        _movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0)];
+        _movieWriter.encodingLiveVideo = YES;
+        [_filter addTarget:_movieWriter];
+        _videoCamera.audioEncodingTarget = _movieWriter;
+        [_movieWriter startRecording];
+        
+        _mLabelTime = 0;
+        _mLabel.hidden = NO;
+        [self onTimer:nil];
+        _mTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(onTimer:) userInfo:nil repeats:YES];
+    }
+    else {
+        [sender setTitle:@"录制" forState:UIControlStateNormal];
+        NSLog(@"End recording");
+        _mLabel.hidden = YES;
+        if (_mTimer) {
+            [_mTimer invalidate];
+        }
+        [_filter removeTarget:_movieWriter];
+        _videoCamera.audioEncodingTarget = nil;
+        [_movieWriter finishRecording];
+        
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(pathToMovie))
+        {
+            [library writeVideoAtPathToSavedPhotosAlbum:movieURL completionBlock:^(NSURL *assetURL, NSError *error)
+             {
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     
+                     if (error) {
+                         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"视频保存失败" message:nil
+                                                                        delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                         [alert show];
+                     } else {
+                         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"视频保存成功" message:nil
+                                                                        delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                         [alert show];
+                     }
+                 });
+             }];
+        }
+        
+    }
+}
+
+- (IBAction)updateSliderValue:(id)sender
+{
+    [(GPUImageSepiaFilter *)_filter setIntensity:[(UISlider *)sender value]];
+}
+
+
+
+@end
---
+++ Tutorial-06/LearnOpenGLESWithGPUImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by 林伟池 on 16/5/10.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
