b28c39bf8938ca31f2ba620c9d2672319b4ac7cf
林伟池 <loying@foxmail.com>
2016-06-13 21:47:21 +0800

    教程5

--- Tutorial-05/LearnOpenGLESWithGPUImage/ViewController.m
+++ Tutorial-05/LearnOpenGLESWithGPUImage/ViewController.m
@@ -8,43 +8,124 @@
 
 #import "ViewController.h"
 #import "GPUImage.h"
+#import <AssetsLibrary/ALAssetsLibrary.h>
 
 @interface ViewController ()
-@property (nonatomic , strong) GPUImagePicture *sourcePicture;
-@property (nonatomic , strong) GPUImageTiltShiftFilter *sepiaFilter;
+@property (nonatomic , strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic , strong) GPUImageOutput<GPUImageInput> *filter;
+@property (nonatomic , strong) GPUImageMovieWriter *movieWriter;
+@property (nonatomic , strong) GPUImageView *filterView;
+
+@property (nonatomic , strong) UIButton *mButton;
+@property (nonatomic , strong) UILabel  *mLabel;
+@property (nonatomic , assign) long     mLabelTime;
+@property (nonatomic , strong) NSTimer  *mTimer;
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    GPUImageView *primaryView = [[GPUImageView alloc] initWithFrame:self.view.frame];
-    self.view = primaryView;
-    UIImage *inputImage = [UIImage imageNamed:@"face.png"];
-    _sourcePicture = [[GPUImagePicture alloc] initWithImage:inputImage];
-    _sepiaFilter = [[GPUImageTiltShiftFilter alloc] init];
-    _sepiaFilter.blurRadiusInPixels = 40.0;
-    [_sepiaFilter forceProcessingAtSize:primaryView.sizeInPixels];
-    [_sourcePicture addTarget:_sepiaFilter];
-    [_sepiaFilter addTarget:primaryView];
-    [_sourcePicture processImage];
     
+    _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    _videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    _videoCamera.horizontallyMirrorFrontFacingCamera = NO;
+    _videoCamera.horizontallyMirrorRearFacingCamera = NO;
     
-    // GPUImageContext相关的数据显示
-    GLint size = [GPUImageContext maximumTextureSizeForThisDevice];
-    GLint unit = [GPUImageContext maximumTextureUnitsForThisDevice];
-    GLint vector = [GPUImageContext maximumVaryingVectorsForThisDevice];
-    NSLog(@"%d %d %d", size, unit, vector);
+    _filter = [[GPUImageSepiaFilter alloc] init];
+    _filterView = [[GPUImageView alloc] initWithFrame:self.view.frame];
+    self.view = _filterView;
+    
+    _mButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 10, 50, 50)];
+    [_mButton setTitle:@"录制" forState:UIControlStateNormal];
+    [_mButton sizeToFit];
+    [self.view addSubview:_mButton];
+    [_mButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _mLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 20, 50, 100)];
+    _mLabel.hidden = YES;
+    _mLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:_mLabel];
+    
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(10, 40, 100, 40)];
+    [slider addTarget:self action:@selector(updateSliderValue:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:slider];
+    
+    [_videoCamera addTarget:_filter];
+    [_filter addTarget:_filterView];
+    [_videoCamera startCameraCapture];
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidChangeStatusBarOrientationNotification object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+        _videoCamera.outputImageOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    }];
+    
 }
 
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    UITouch* touch = [touches anyObject];
-    CGPoint point = [touch locationInView:self.view];
-    float rate = point.y / self.view.frame.size.height;
-    NSLog(@"Processing");
-    [_sepiaFilter setTopFocusLevel:rate - 0.1];
-    [_sepiaFilter setBottomFocusLevel:rate + 0.1];
-    [_sourcePicture processImage];
+
+
+- (void)onTimer:(id)sender {
+    _mLabel.text  = [NSString stringWithFormat:@"录制时间:%lds", _mLabelTime++];
+    [_mLabel sizeToFit];
 }
+
+- (void)onClick:(UIButton *)sender {
+    NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie4.m4v"];
+    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+    if([sender.currentTitle isEqualToString:@"录制"]) {
+        [sender setTitle:@"结束" forState:UIControlStateNormal];
+        NSLog(@"Start recording");
+        unlink([pathToMovie UTF8String]); // 如果已经存在文件，AVAssetWriter会有异常，删除旧文件
+        _movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0)];
+        _movieWriter.encodingLiveVideo = YES;
+        [_filter addTarget:_movieWriter];
+        _videoCamera.audioEncodingTarget = _movieWriter;
+        [_movieWriter startRecording];
+        
+        _mLabelTime = 0;
+        _mLabel.hidden = NO;
+        [self onTimer:nil];
+        _mTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(onTimer:) userInfo:nil repeats:YES];
+    }
+    else {
+        [sender setTitle:@"录制" forState:UIControlStateNormal];
+        NSLog(@"End recording");
+        _mLabel.hidden = YES;
+        if (_mTimer) {
+            [_mTimer invalidate];
+        }
+        [_filter removeTarget:_movieWriter];
+        _videoCamera.audioEncodingTarget = nil;
+        [_movieWriter finishRecording];
+        
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(pathToMovie))
+        {
+            [library writeVideoAtPathToSavedPhotosAlbum:movieURL completionBlock:^(NSURL *assetURL, NSError *error)
+             {
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     
+                     if (error) {
+                         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"视频保存失败" message:nil
+                                                                        delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                         [alert show];
+                     } else {
+                         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"视频保存成功" message:nil
+                                                                        delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                         [alert show];
+                     }
+                 });
+             }];
+        }
+        
+    }
+}
+
+- (IBAction)updateSliderValue:(id)sender
+{
+    [(GPUImageSepiaFilter *)_filter setIntensity:[(UISlider *)sender value]];
+}
+
+
 
 @end
