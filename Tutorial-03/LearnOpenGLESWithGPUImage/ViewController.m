31efdf7e001a2e64c75ea6e442139335e19c7a64
林伟池 <loying@foxmail.com>
2016-06-21 10:28:28 +0800

    教程3

--- Tutorial-03/LearnOpenGLESWithGPUImage/ViewController.m
+++ Tutorial-03/LearnOpenGLESWithGPUImage/ViewController.m
@@ -8,98 +8,52 @@
 
 #import "ViewController.h"
 #import "GPUImage.h"
+#import "GPUImageBeautifyFilter.h"
 #import <AssetsLibrary/ALAssetsLibrary.h>
 
 @interface ViewController ()
 @property (nonatomic , strong) UILabel  *mLabel;
+
+@property (nonatomic, strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic , strong) GPUImageMovieWriter *movieWriter;
+@property (nonatomic, strong) GPUImageView *filterView;
+
 @end
 
 
 @implementation ViewController
 {
-    GPUImageMovie *movieFile;
-    GPUImageOutput<GPUImageInput> *filter;
-    GPUImageMovieWriter *movieWriter;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    GPUImageView *filterView = [[GPUImageView alloc] initWithFrame:self.view.frame];
-    self.view = filterView;
     
-    self.mLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
-    self.mLabel.textColor = [UIColor redColor];
-    [self.view addSubview:self.mLabel];
+    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+    self.videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    self.videoCamera.horizontallyMirrorFrontFacingCamera = YES;
+    self.filterView = [[GPUImageView alloc] initWithFrame:self.view.frame];
+    self.filterView.center = self.view.center;
+    [self.view addSubview:self.filterView];
     
-    // 滤镜
-    filter = [[GPUImageDissolveBlendFilter alloc] init];
-    [(GPUImageDissolveBlendFilter *)filter setMix:0.5];
     
-    // 播放
-    NSURL *sampleURL = [[NSBundle mainBundle] URLForResource:@"abc" withExtension:@"mp4"];
-    AVAsset *asset = [AVAsset assetWithURL:sampleURL];
-    CGSize size = self.view.bounds.size;
-    movieFile = [[GPUImageMovie alloc] initWithAsset:asset];
-    movieFile.runBenchmark = YES;
-    movieFile.playAtActualSpeed = YES;
-    
-    // 水印
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
-    label.text = @"我是水印";
-    label.font = [UIFont systemFontOfSize:30];
-    label.textColor = [UIColor redColor];
-    [label sizeToFit];
-    UIImage *image = [UIImage imageNamed:@"watermark.png"];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-    UIView *subView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
-    subView.backgroundColor = [UIColor clearColor];
-    imageView.center = CGPointMake(subView.bounds.size.width / 2, subView.bounds.size.height / 2);
-    [subView addSubview:imageView];
-    [subView addSubview:label];
-    
-    GPUImageUIElement *uielement = [[GPUImageUIElement alloc] initWithView:subView];
-    
-//    GPUImageTransformFilter 动画的filter
     NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie.m4v"];
     unlink([pathToMovie UTF8String]);
     NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
     
-    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(640.0, 480.0)];
+    _movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(640.0, 480.0)];
     
-    GPUImageFilter* progressFilter = [[GPUImageFilter alloc] init];
-    [movieFile addTarget:progressFilter];
-    [progressFilter addTarget:filter];
-    [uielement addTarget:filter];
-    movieWriter.shouldPassthroughAudio = YES;
-    movieFile.audioEncodingTarget = movieWriter;
-    [movieFile enableSynchronizedEncodingUsingMovieWriter:movieWriter];
-    // 显示到界面
-    [filter addTarget:filterView];
-    [filter addTarget:movieWriter];
-
+    self.videoCamera.audioEncodingTarget = _movieWriter;
+    _movieWriter.encodingLiveVideo = YES;
+    [self.videoCamera startCameraCapture];
+    GPUImageBeautifyFilter *beautifyFilter = [[GPUImageBeautifyFilter alloc] init];
+    [self.videoCamera addTarget:beautifyFilter];
+    [beautifyFilter addTarget:self.filterView];
+    [beautifyFilter addTarget:_movieWriter];
+    [_movieWriter startRecording];
     
-    [movieWriter startRecording];
-    [movieFile startProcessing];
-    
-    
-    CADisplayLink* dlink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateProgress)];
-    [dlink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
-    [dlink setPaused:NO];
-    
-    __weak typeof(self) weakSelf = self;
-    
-    [progressFilter setFrameProcessingCompletionBlock:^(GPUImageOutput *output, CMTime time) {
-        CGRect frame = imageView.frame;
-        frame.origin.x += 1;
-        frame.origin.y += 1;
-        imageView.frame = frame;
-        [uielement updateWithTimestamp:time];
-    }];
-    
-    [movieWriter setCompletionBlock:^{
-        __strong typeof(self) strongSelf = weakSelf;
-        [strongSelf->filter removeTarget:strongSelf->movieWriter];
-        [strongSelf->movieWriter finishRecording];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [beautifyFilter removeTarget:_movieWriter];
+        [_movieWriter finishRecording];
         ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
         if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(pathToMovie))
         {
@@ -122,21 +76,7 @@
         else {
             NSLog(@"error mssg)");
         }
-    }];
-}
-
-
-
-- (void)updateProgress
-{
-    self.mLabel.text = [NSString stringWithFormat:@"Progress:%d%%", (int)(movieFile.progress * 100)];
-    [self.mLabel sizeToFit];
-}
-
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    });
 }
 
 
