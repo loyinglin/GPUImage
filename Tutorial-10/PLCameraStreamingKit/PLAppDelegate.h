49fddc4a2640d7b0ba54bf5589eefcf06144d902
林伟池 <loying@foxmail.com>
2016-07-22 10:08:31 +0800

    七牛云demo

---
+++ Tutorial-10/PLCameraStreamingKit/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  PLCameraStreamingKit
+//
+//  Created on 01/10/2015.
+//  Copyright (c) Pili Engineering, Qiniu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tutorial-10/PLCameraStreamingKit/PLAppDelegate.m
@@ -0,0 +1,53 @@
+//
+//  PLAppDelegate.m
+//  PLCameraStreamingKit
+//
+//  Created on 01/10/2015.
+//  Copyright (c) Pili Engineering, Qiniu Inc. All rights reserved.
+//
+
+#import "PLAppDelegate.h"
+#import <PLStreamingKit/PLStreamingEnv.h>
+#import <KSCrash/KSCrashInstallationStandard.h>
+
+@implementation PLAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    KSCrashInstallationStandard* installation = [KSCrashInstallationStandard sharedInstance];
+    installation.url = [NSURL URLWithString:@"https://collector.bughd.com/kscrash?key=a4ae5d97b578c3b2bc7b0e15319cda1e"];
+    [installation install];
+    [installation sendAllReportsWithCompletion:nil];
+    [PLStreamingEnv initEnv];
+    // Override point for customization after application launch.
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
