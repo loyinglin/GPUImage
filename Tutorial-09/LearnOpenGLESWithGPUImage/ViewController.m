4174062d8d16f0d40a3b9824ecd29c6ec504177b
林伟池 <loying@foxmail.com>
2016-08-03 10:56:10 +0800

    教程9⃣️

--- Tutorial-09/LearnOpenGLESWithGPUImage/ViewController.m
+++ Tutorial-09/LearnOpenGLESWithGPUImage/ViewController.m
@@ -30,7 +30,7 @@
     self.mImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     [self.view addSubview:self.mImageView];
     
-    self.mLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
+    self.mLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 200, 100)];
     self.mLabel.textColor = [UIColor redColor];
     [self.view addSubview:self.mLabel];
     
@@ -40,8 +40,6 @@
    
     self.mOutput = [[GPUImageRawDataOutput alloc] initWithImageSize:CGSizeMake(640, 480) resultsInBGRAFormat:YES];
     
-
-//    [videoCamera addTarget:filterView];
     [videoCamera addTarget:self.mOutput];
     
     
@@ -59,8 +57,9 @@
         if (ret != kCVReturnSuccess) {
             NSLog(@"status %d", ret);
         }
+//        [ViewController convertBGRAtoRGBA:strongOutput.rawBytesForImage withSize:bytesPerRow * 480];
 //        NSData* data = [[NSData alloc] initWithBytes:strongOutput.rawBytesForImage length:bytesPerRow * 480];
-//        UIImage *image = [[UIImage alloc] initWithData:data];
+//        UIImage *uiimage = [[UIImage alloc] initWithData:data];
         
         [strongOutput unlockFramebufferAfterReading];
         if(pixelBuffer == NULL) {
@@ -69,9 +68,10 @@
         CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
         CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, strongOutput.rawBytesForImage, bytesPerRow * 480, NULL);
         
-        CGImageRef cgImage = CGImageCreate(640, 480, 8, 32, bytesPerRow, rgbColorSpace, kCGImageAlphaFirst|kCGBitmapByteOrder32Little, provider, NULL, true, kCGRenderingIntentDefault);
+        CGImageRef cgImage = CGImageCreate(640, 480, 8, 32, bytesPerRow, rgbColorSpace, kCGImageAlphaPremultipliedFirst|kCGBitmapByteOrder32Little, provider, NULL, true, kCGRenderingIntentDefault);
         UIImage *image = [UIImage imageWithCGImage:cgImage];
         [strongSelf updateWithImage:image];
+//        NSData *pngData = UIImagePNGRepresentation(image);
         
         CGImageRelease(cgImage);
         CFRelease(pixelBuffer);
@@ -87,6 +87,15 @@
     [dlink setPaused:NO];
 }
 
++ (void)convertBGRAtoRGBA:(unsigned char *)data withSize:(size_t)sizeOfData {
+    for (unsigned char *p = data; p < data + sizeOfData; p += 4) {
+        unsigned char r = *(p + 2);
+        unsigned char b = *p;
+        *p = r;
+        *(p + 2) = b;
+    }
+}
+
 
 - (void)updateWithImage:(UIImage *)image {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -96,6 +105,8 @@
 
 - (void)updateProgress
 {
+    self.mLabel.text = [[NSDate dateWithTimeIntervalSinceNow:0] description];
+    [self.mLabel sizeToFit];
 }
 
 
