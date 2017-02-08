7a0cdfa06a096816ec8e0f9fb7dd824d3c58aece
林伟池 <loying@foxmail.com>
2017-02-08 09:33:46 +0800

    sobel边界探测

---
+++ Tutorial-sobel/LearnOpenGLESWithGPUImage/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by 林伟池 on 16/5/10.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GPUImage.h"
+
+@interface ViewController ()
+@property (nonatomic , strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic , strong) GPUImageSobelEdgeDetectionFilter *filter;
+@property (nonatomic , strong) GPUImageMovieWriter *movieWriter;
+@property (nonatomic , strong) GPUImageView *filterView;
+
+@property (nonatomic , strong) CADisplayLink *mDisplayLink;
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    _videoCamera.outputImageOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    
+    _filter = [[GPUImageSobelEdgeDetectionFilter alloc] init];
+    _filterView = [[GPUImageView alloc] initWithFrame:self.view.frame];
+    self.view = _filterView;
+            
+    [_videoCamera addTarget:_filter];
+    [_filter addTarget:_filterView];
+    [_videoCamera startCameraCapture];
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidChangeStatusBarOrientationNotification object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+        _videoCamera.outputImageOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    }];
+    
+    self.mDisplayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displaylink:)];
+    [self.mDisplayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+- (void)displaylink:(CADisplayLink *)displaylink {
+}
+
+
+
+
+@end
