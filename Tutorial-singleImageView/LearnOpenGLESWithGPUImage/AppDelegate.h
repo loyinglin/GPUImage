1284224635c32390c1503a17224eed6d7203e3a1
loyinglin <loying@foxmail.com>
2018-05-23 14:30:48 +0800

    测试。

---
+++ Tutorial-singleImageView/LearnOpenGLESWithGPUImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by 林伟池 on 16/5/10.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
