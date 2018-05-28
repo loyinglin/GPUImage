7af187cf850faba2b6232f89d04329d858cc350f
loyinglin <loying@foxmail.com>
2018-05-28 14:19:12 +0800

    生产环境。

---
+++ forTest/product/LearnOpenGLESWithGPUImage/LYAssetReader.m
@@ -0,0 +1,77 @@
+//
+//  LYAssetReader.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by loyinglin on 2018/5/25.
+//  Copyright © 2018年 林伟池. All rights reserved.
+//
+
+#import "LYAssetReader.h"
+
+
+@implementation LYAssetReader
+{
+    AVAssetReaderTrackOutput *readerVideoTrackOutput;
+    AVAssetReader   *assetReader;
+    NSURL *videoUrl;
+}
+
+- (instancetype)initWithUrl:(NSURL *)url {
+    self = [super init];
+    videoUrl = url;
+    [self customInit];
+    return self;
+}
+
+- (void)customInit {
+    NSDictionary *inputOptions = [NSDictionary dictionaryWithObject:[NSNumber numberWithBool:YES] forKey:AVURLAssetPreferPreciseDurationAndTimingKey];
+    AVURLAsset *inputAsset = [[AVURLAsset alloc] initWithURL:videoUrl options:inputOptions];
+    __weak typeof(self) weakSelf = self;
+    [inputAsset loadValuesAsynchronouslyForKeys:[NSArray arrayWithObject:@"tracks"] completionHandler: ^{
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            NSError *error = nil;
+            AVKeyValueStatus tracksStatus = [inputAsset statusOfValueForKey:@"tracks" error:&error];
+            if (tracksStatus != AVKeyValueStatusLoaded)
+            {
+                NSLog(@"error %@", error);
+                return;
+            }
+            [weakSelf processWithAsset:inputAsset];
+        });
+    }];
+}
+
+- (void)processWithAsset:(AVAsset *)asset
+{
+    NSError *error = nil;
+    assetReader = [AVAssetReader assetReaderWithAsset:asset error:&error];
+    
+    NSMutableDictionary *outputSettings = [NSMutableDictionary dictionary];
+    
+    [outputSettings setObject:@(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+    
+    readerVideoTrackOutput = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:[[asset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] outputSettings:outputSettings];
+    readerVideoTrackOutput.alwaysCopiesSampleData = NO;
+    [assetReader addOutput:readerVideoTrackOutput];
+
+    
+    if ([assetReader startReading] == NO)
+    {
+        NSLog(@"Error reading from file at URL: %@", asset);
+        return;
+    }
+}
+
+- (CMSampleBufferRef)readBuffer {
+    CMSampleBufferRef sampleBufferRef = [readerVideoTrackOutput copyNextSampleBuffer];
+    
+    if (assetReader.status == AVAssetReaderStatusCompleted) {
+        [self customInit];
+    }
+    
+    return sampleBufferRef;
+}
+
+
+@end
+
---
+++ forTest/product/LearnOpenGLESWithGPUImage/ViewController.m
@@ -0,0 +1,142 @@
+//
+//  ViewController.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by 林伟池 on 16/5/10.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GPUImage.h"
+#import "LYMultiTextureFilter.h"
+#import "LYAssetReader.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) NSMutableArray<GPUImageView *> *gpuImageViewArray;
+@property (nonatomic, strong) NSMutableArray<GPUImageMovie *> *gpuImageMovieArray;
+
+@property (nonatomic, strong) NSMutableArray<LYAssetReader *> *lyReaderArray;
+
+
+@property (nonatomic, strong) CADisplayLink *mDisplayLink;
+
+@property (nonatomic, strong) LYMultiTextureFilter *lyMultiTextureFilter;
+@end
+
+
+@implementation ViewController
+
+#define MaxRow (2)
+#define MaxColumn (3)
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.gpuImageViewArray = [[NSMutableArray<GPUImageView *> alloc] init];
+    self.gpuImageMovieArray = [[NSMutableArray<GPUImageMovie *> alloc] init];
+    self.lyReaderArray = [[NSMutableArray<LYAssetReader *> alloc] init];
+    NSArray *fileNamesArray = @[@"abc", @"qwe", @"abc", @"qwe", @"abc", @"qwe"];
+    
+    self.lyMultiTextureFilter = [[LYMultiTextureFilter alloc] initWithMaxFilter:MaxRow * MaxColumn];
+    for (int indexRow = 0; indexRow < MaxRow; ++indexRow) {
+        for (int indexColumn = 0; indexColumn < MaxColumn; ++indexColumn) {
+            CGRect frame = CGRectMake(indexColumn * 1.0 / MaxColumn,
+                                      indexRow * 1.0 / MaxRow,
+                                      1.0 / MaxColumn,
+                                      1.0 / MaxRow);
+            NSURL *videoUrl = [[NSBundle mainBundle] URLForResource:fileNamesArray[indexRow * MaxColumn + indexColumn] withExtension:@"mp4"];
+            GPUImageMovie *movie = [[GPUImageMovie alloc] initWithURL:videoUrl];
+            [self buildGPUImageViewWithFrame:frame imageMovie:movie];
+            [self.gpuImageMovieArray addObject:movie];
+            
+            LYAssetReader *reader = [[LYAssetReader alloc] initWithUrl:videoUrl];
+            [self.lyReaderArray addObject:reader];
+        }
+    }
+    GPUImageView *imageView = [[GPUImageView alloc] initWithFrame:CGRectMake(0, 100, CGRectGetWidth(self.view.bounds), CGRectGetWidth(self.view.bounds) / MaxColumn * MaxRow)];
+    imageView.fillMode = kGPUImageFillModeStretch;
+    
+    [self.lyMultiTextureFilter addTarget:imageView];
+    [self.view addSubview:imageView];
+    
+}
+
+
+- (IBAction)onClick:(id)sender {
+    if (!self.mDisplayLink) {
+        self.mDisplayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displaylink:)];
+        self.mDisplayLink.frameInterval = 2;
+        [self.mDisplayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    }
+}
+
+
+- (void)buildGPUImageViewWithFrame:(CGRect)frame imageMovie:(GPUImageMovie *)imageMovie {
+    // 1080 1920，这里已知视频的尺寸。如果不清楚视频的尺寸，可以先读取视频帧CVPixelBuffer，再用CVPixelBufferGetHeight/Width
+    GPUImageCropFilter *cropFilter = [[GPUImageCropFilter alloc] initWithCropRegion:CGRectMake((1920 - 1080) / 2 / 1920, 0, 1080.0 / 1920, 1)];
+    
+    GPUImageTransformFilter *transformFilter = [[GPUImageTransformFilter alloc] init];
+    transformFilter.affineTransform = CGAffineTransformMakeRotation(M_PI_2);
+    
+    GPUImageOutput *tmpFilter;
+    
+    tmpFilter = imageMovie;
+    
+    [tmpFilter addTarget:cropFilter];
+    tmpFilter = cropFilter;
+    
+    [tmpFilter addTarget:transformFilter];
+    tmpFilter = transformFilter;
+    
+    NSInteger index = [self.lyMultiTextureFilter nextAvailableTextureIndex];
+    [tmpFilter addTarget:self.lyMultiTextureFilter atTextureLocation:index];
+    [self.lyMultiTextureFilter setDrawRect:frame atIndex:index];
+    
+    return ;
+}
+
+- (void)displaylink:(CADisplayLink *)displaylink {
+    for (int index = 0; index < MaxRow * MaxColumn; ++index) {
+        GPUImageMovie *imageMovie = self.gpuImageMovieArray[index];
+        LYAssetReader *reader = self.lyReaderArray[index];
+        
+        static BOOL lyTest = NO;
+        if (index == 3 && lyTest) {
+            static BOOL hadRefresh = NO;
+            if (!hadRefresh) {
+                hadRefresh = YES;
+                [self.lyMultiTextureFilter clearDrawRect:[self getFrameByIndex:index rowNum:MaxRow columnNum:MaxColumn]];
+            }
+            continue;
+        }
+        
+        CMSampleBufferRef sampleBufferRef = [reader readBuffer];
+        if (sampleBufferRef)
+        {
+            runSynchronouslyOnVideoProcessingQueue(^{
+                [imageMovie processMovieFrame:sampleBufferRef];
+                CMSampleBufferInvalidate(sampleBufferRef);
+                CFRelease(sampleBufferRef);
+            });
+        }
+    }
+}
+
+
+
+
+- (CGRect)getFrameByIndex:(int)index rowNum:(int)rowNum columnNum:(int)columnNum {
+    NSAssert(index < rowNum * columnNum, @"getFrameByIndex error");
+    int row = (index + 1) / columnNum;
+    int column = index % columnNum;
+    CGRect frame = CGRectMake(column * 1.0 / columnNum,
+                              row * 1.0 / rowNum,
+                              1.0 / columnNum,
+                              1.0 / rowNum);
+    return frame;
+}
+
+
+
+@end
