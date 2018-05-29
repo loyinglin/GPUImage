1ce9441f4707424ab51bb07b0c95c962a31a0c11
loyinglin <loying@foxmail.com>
2018-05-29 16:32:59 +0800

    datamananger

---
+++ forTest/product/LearnOpenGLESWithGPUImage/KSDataManager.h
@@ -0,0 +1,14 @@
+//
+//  KSDataManager.h
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by loyinglin on 2018/5/28.
+//  Copyright © 2018年 林伟池. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSDataManager : NSObject
+
+
+@end
---
+++ forTest/product/LearnOpenGLESWithGPUImage/KSDataManager.m
@@ -0,0 +1,13 @@
+//
+//  KSDataManager.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by loyinglin on 2018/5/28.
+//  Copyright © 2018年 林伟池. All rights reserved.
+//
+
+#import "KSDataManager.h"
+
+@implementation KSDataManager
+
+@end
