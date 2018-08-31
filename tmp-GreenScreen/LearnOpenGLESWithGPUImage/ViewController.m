b03d3ca5ce17107abf4fc2f0146fcd2258bf0961
loyinglin <loying@foxmail.com>
2018-08-31 18:05:09 +0800

    绿幕。

---
+++ tmp-GreenScreen/LearnOpenGLESWithGPUImage/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  LearnOpenGLESWithGPUImage
+//
+//  Created by 林伟池 on 16/5/10.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GPUImage.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) GPUImageMovie *movieGreen;
+@property (nonatomic, strong) GPUImageMovie *movieNormal;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.movieGreen = [[GPUImageMovie alloc] initWithURL:[[NSBundle mainBundle] URLForResource:@"greenscreen" withExtension:@"mp4"]];
+    self.movieGreen.playAtActualSpeed = YES;
+    self.movieGreen.shouldRepeat = YES;
+    
+    self.movieNormal = [[GPUImageMovie alloc] initWithURL:[[NSBundle mainBundle] URLForResource:@"abc" withExtension:@"mp4"]];
+    self.movieNormal.playAtActualSpeed = YES;
+    self.movieNormal.shouldRepeat = YES;
+    
+    GPUImageChromaKeyBlendFilter *chromaKeyBlendFilter = [[GPUImageChromaKeyBlendFilter alloc] init];
+    
+    GPUImageView *imageView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:imageView];
+    
+    // chan
+    [self.movieGreen addTarget:chromaKeyBlendFilter];
+    [self.movieNormal addTarget:chromaKeyBlendFilter];
+    [chromaKeyBlendFilter addTarget:imageView];
+
+    [self.movieGreen startProcessing];
+    [self.movieNormal startProcessing];
+    
+}
+
+@end
